190a0d9581ef483b9a8c01374fc33e06772819a1
llit0 <llit0show@gmail.com>
2021-02-28 14:48:26 +0800

    Temple

---
+++ Assets/Scripts/FogEmitter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogEmitter : MonoBehaviour
+{   
+    ParticleSystem fogParticles;
+    private void Awake() 
+    {
+        fogParticles = this.gameObject.GetComponent<ParticleSystem>();
+        if(!fogParticles.isPlaying)
+            fogParticles.Play();
+    }
+
+    void Update()
+    {
+        if(!fogParticles.isPlaying)
+            fogParticles.Play();
+    }
+}
