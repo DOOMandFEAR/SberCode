ea65c9721ca74a703919d678f65daa3ce4c2d55d
llit0 <llit0show@gmail.com>
2021-02-28 19:45:59 +0800

    d

---
+++ Assets/Scripts/CameraInit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraInit : MonoBehaviour
+{
+    bool setPos = false;
+    void Start()
+    {
+        this.transform.position = new Vector3(6.6f, 1.5f, -2.7f);       
+    }
+
+    private void Update() {
+        if(this.transform.position != new Vector3(6.6f, 1.5f, -2.7f) && !setPos)
+        {
+            this.transform.position = new Vector3(6.6f, 1.5f, -2.7f);
+            setPos = true;
+        }
+    }
+}
